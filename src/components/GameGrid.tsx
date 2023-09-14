9d073ad3476519ad24269adc3b8c6f9be4ae19ee
Godfredasare <godasare8346@gmail.com>
2023-09-14 20:49:58 +0000

    Show loading skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -3,6 +3,7 @@ import apiClient from "../services/api-client";
 import { AxiosError } from "axios";
 import { SimpleGrid } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeleton from "./GameCardSkeleton";
 
 export interface Platform {
   id: number,
@@ -26,13 +27,17 @@ interface FetchGame {
 const GameGrid = () => {
   const [game, setGame] = useState<Game[]>([]);
   const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false)
 
   const fetchGames = async () => {
+    setLoading(true)
     try {
       const res = await apiClient.get<FetchGame>("/games");
       setGame(res.data.results);
+      setLoading(false)
     } catch (error) {
       setError((error as AxiosError).message);
+      setLoading(false)
     }
   };
 
@@ -40,10 +45,13 @@ const GameGrid = () => {
     fetchGames();
   }, []);
 
+  let Skeleton = [1, 2, 3, 4, 5, 6]
+
   return (
     <>
       {error && <p>{error}</p>}
       <SimpleGrid columns={{sm: 1, md: 2, lg: 3,}} spacing={10} padding={'10px'}>
+        {isLoading && Skeleton.map((e) => <GameCardSkeleton key={e} />)}
         {game.map((e) => (
          <GameCard key={e.id} games={e}/>
         ))}
